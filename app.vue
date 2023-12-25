3456f266dc2045720aa728c09129efa29de95ded
MSI\gn01792218 <gn01792218@hotmail.com>
2023-12-25 12:01:29 +0800

    [feature] setup pinia and tailwindcss

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-black text-white">
+    <p>HI~~~這是測試TailwindCss的頁面</p>
+    <button @click="increment">++</button>
+    <p>{{ count }}</p>
+    <p>2倍 {{ doubleCount }}</p>
+    <NuxtWelcome />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { useExampleStore } from './store/exampleStore'
+  const { increment } = useExampleStore()
+  const { count, doubleCount } = storeToRefs(useExampleStore())
+
+</script>
