842628ecb75bd018bb912a56c41fa4d57c392b48
MSI\gn01792218 <gn01792218@hotmail.com>
2024-01-31 19:00:17 +0800

    [rollback] rollback

--- server/api/myModels/index.get.ts
+++ server/api/myModels/index.get.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from "@prisma/client"
-import myModels from "~/data/model"
 
 const prisma = new PrismaClient()
 export default defineEventHandler(async(event)=>{
     return await prisma.myModel.findMany({
-        // include:{
-        //     finish_info:true,
-        // }
+        include:{
+            finish_info:true,
+        }
     })
 })
