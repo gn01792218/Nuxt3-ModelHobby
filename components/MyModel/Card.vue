6a87eb2ba4290e754d2d52d1133bf2e6931e45bd
MSI\gn01792218 <gn01792218@hotmail.com>
2024-01-31 20:20:29 +0800

    [feature] update model card style

--- components/MyModel/Card.vue
+++ components/MyModel/Card.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="flex justify-center items-center">
-    <div class="cursor-pointer" @click="navigateTo(`MyModel/ModelDetail-${modelData.id}`)">
+  <UCard class="bg-red-100">
+    <template #header>
+      <p>{{ modelData.name_zh }}</p>
+      <p>{{ modelData.name_en }}</p>
+      <p v-if="modelData.scale">{{ modelData.scale }}</p>
+    </template>
+    <div>
+      <div class="cursor-pointer" @click="navigateTo(`MyModel/ModelDetail-${modelData.id}`)">
         <img v-if="modelData.main_img" :src="getModelImagePublicUrl(modelData.main_img)" :alt="modelData.name_zh">
         <img v-else src="" alt="沒有上傳圖片">
+      </div>
     </div>
-    <div>
-        <p>{{ modelData.name_zh }}</p>
-        <p>{{ modelData.name_en }}</p>
-        <p v-if="modelData.scale">{{ modelData.scale }}</p>
-    </div>
-    <div>
-      <button class="border-2 p-5" @click="navigateTo(`MyModel/ModelDetail-${modelData.id}`)">查看</button>
-      <button v-if="user?.id ==='ba5171d3-299b-4f64-983b-7faf1621944d' || user?.id === 'a161fb29-6948-4f8c-94c9-1ac707f5dac1'" class="border-2 p-5" @click="fetchDeleteMyModel"> - 刪除</button>
-    </div>
-  </div>
+    <template #footer>
+      <div>
+          <UButton class="mr-5" icon="i-heroicons-pencil-square" size="sm" color="green" variant="soft"
+            :trailing="false" @click="navigateTo(`MyModel/ModelDetail-${modelData.id}`)"/>
+          <UButton
+                v-if="user?.id === 'ba5171d3-299b-4f64-983b-7faf1621944d' || user?.id === 'a161fb29-6948-4f8c-94c9-1ac707f5dac1'"
+            icon="i-heroicons-trash-solid"
+            size="sm"
+            color="red"
+            variant="solid"
+            :trailing="false"
+            @click="fetchDeleteMyModel"
+          />
+      </div>
+    </template>
+  </UCard>
 </template>
 
 <script setup lang="ts">
@@ -26,7 +39,7 @@ import model from "~/data/model"
 
 
 const props = defineProps<{
-    modelData:Model
+  modelData: Model
 }>()
 const { getModelImagePublicUrl, removeImageFromSupabaseStorage } = useSupabase()
 const { deleteMyModel } = useFetchMyModels()
@@ -34,23 +47,23 @@ const { setmyModelList, setLoadingState } = useMyModelStore()
 const { myModelList } = storeToRefs(useMyModelStore())
 const user = useSupabaseUser()
 const supabase = useSupabaseClient()
-function fetchDeleteImg(){
+function fetchDeleteImg() {
   const { modelData } = props
   //刪除mainimg
-  if(modelData.main_img) removeImageFromSupabaseStorage(StorageBucket.images,modelData.main_img)
+  if (modelData.main_img) removeImageFromSupabaseStorage(StorageBucket.images, modelData.main_img)
   //刪除完成資訊的兩種img
-  if(modelData.finish_info?.process_imgs?.length) {
-    modelData.finish_info.process_imgs.forEach(img=>removeImageFromSupabaseStorage(StorageBucket.model_finish_info_images,img))
+  if (modelData.finish_info?.process_imgs?.length) {
+    modelData.finish_info.process_imgs.forEach(img => removeImageFromSupabaseStorage(StorageBucket.model_finish_info_images, img))
   }
-  if(modelData.finish_info?.gallery?.length) {
-    modelData.finish_info.gallery.forEach(img=>removeImageFromSupabaseStorage(StorageBucket.model_finish_info_images,img))
+  if (modelData.finish_info?.gallery?.length) {
+    modelData.finish_info.gallery.forEach(img => removeImageFromSupabaseStorage(StorageBucket.model_finish_info_images, img))
   }
-} 
-async function fetchDeleteMyModel(){
+}
+async function fetchDeleteMyModel() {
   setLoadingState(true)
   await deleteMyModel(props.modelData.id!)
   fetchDeleteImg()
-  setmyModelList(myModelList.value.filter(model=>model.id !== props.modelData.id))
+  setmyModelList(myModelList.value.filter(model => model.id !== props.modelData.id))
   setLoadingState(false)
 }
 </script>
