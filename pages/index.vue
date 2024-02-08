72f356fdd601e0a7fcd0bd6c00c8eb43c923c76f
MSI\gn01792218 <gn01792218@hotmail.com>
2024-02-08 20:49:17 +0800

    [feature] add cal total coast

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>總花費 : ????</p>
+        <p>總花費 : {{ totalCoast }} 台幣</p>
         <p>目前共有{{ myModelList.length }} 個模型</p>
         <p>未入庫:{{ unStockInModels.length }}個</p>
         <p>未組裝:{{ unFinishedModels.length }}個</p>
@@ -8,6 +8,7 @@
     </div>
 </template>
 <script setup lang="ts">
+import { Currency } from '~/types/model'
 import { useMyModelStore } from '../store/useMyModelStore'
 const {
     myModelList,
@@ -15,4 +16,14 @@ const {
     unFinishedModels,
     finishedModels,
 } = storeToRefs(useMyModelStore())
+const totalCoast = computed(()=>{
+    let total =0
+    myModelList.value.forEach(model=>{
+        model.purchase_infos?.forEach(info=>{
+            if(info.currency === Currency.RMB) total+= info.price*4.5*info.amount
+            else total += info.price*info.amount
+        })
+    })
+    return total
+})
 </script>
