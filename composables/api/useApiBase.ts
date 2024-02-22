da8c82bd6e4b7a84bf8da7c8aa3b761b31b95825
MSI\gn01792218 <gn01792218@hotmail.com>
2024-02-22 09:42:14 +0800

    [fix] fix not cancle loading when throw error

--- composables/api/useApiBase.ts
+++ composables/api/useApiBase.ts
@@ -1,6 +1,9 @@
+import { useMyModelStore } from "~/store/useMyModelStore";
+
 export default () => {
   const toast = useToast()
   const { apiBaseUrl } = useRuntimeConfig().public
+  const { setLoadingState } = useMyModelStore()
 
   async function fetchApiBase(url:string, method:'post' | 'get' | 'delete' | 'patch' | 'put',body?: any): Promise<any> {
     const { data, error } = await useFetch(url, {
@@ -10,11 +13,12 @@ export default () => {
     });
     if (error.value) {
       toast.add({ 
-        title:'請求發生錯誤',
+        title:'請求發生錯誤,請稍後再嘗試',
         description:`${error.value.message}`,
         icon:'i-heroicons-exclamation-circle-16-solid',
         timeout:0
        })
+       setLoadingState(false)
       throw createError({ ...error.value, message: "資料異常" });
     }
     return data.value as unknown as any;
