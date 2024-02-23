4da0fab6674ab0f8c90a602087d8b93b53e2ac5d
MSI\gn01792218 <gn01792218@hotmail.com>
2024-02-23 14:01:53 +0800

    [fix] fix toast

--- composables/useMyToast.ts
+++ composables/useMyToast.ts
@@ -3,7 +3,7 @@ import type { ToastOptions } from "~/types/toast";
 export default () => {
   const toast = useToast()
   function sendToast(option:ToastOptions){
-    
+   toast.add(option as Partial<Notification>) 
   }
 
   return {
