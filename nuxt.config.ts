6b1c8d16f160e22dd386b617a9e67587766e5478
MSI\gn01792218 <gn01792218@hotmail.com>
2024-03-07 09:09:52 +0800

    [feature] disable mobile scale websit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css:['~/assets/css/style.css'],
   supabase:{
     redirect:false,  //true的話就會設置路由守衛，自動把尚未登入者導到login
   },
