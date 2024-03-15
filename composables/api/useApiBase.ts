8f12aa39d00fec36a809d021047edc68f8077dc2
MSI\gn01792218 <gn01792218@hotmail.com>
2024-03-15 12:54:28 +0800

    [fix] fix endless auto request when data change

--- composables/api/useApiBase.ts
+++ composables/api/useApiBase.ts
@@ -10,18 +10,18 @@ export default () => {
     const { data, error } = await useFetch(url, {
       baseURL:apiBaseUrl,
       method,
-      body
+      body:JSON.stringify(body)
     });
     if (error.value) {
       sendToast({ 
         title:'請求發生錯誤,請稍後再嘗試',
-        description:`${error.value.message}`,
+        description:`${error.value.response?._data.message}`,
         icon:'i-heroicons-exclamation-circle-16-solid',
         color:'red',
         timeout:0
        })
        setLoadingState(false)
-      throw createError({ ...error.value, message: "資料異常" });
+      throw createError({ ...error.value, message:error.value.message });
     }
     return data.value as unknown as any;
   }
