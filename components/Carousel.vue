449c7f94bfaefdcb466f8b0a2095f874b9f1bb12
MSI\gn01792218 <gn01792218@hotmail.com>
2024-03-23 13:28:00 +0800

    [fix] fix nuxtUI version bug

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-    <UCarousel v-if="modelList" v-slot="{ item }" :items="modelList" :ui="{ item: 'basis-full md:basis-1/2 lg:basis-1/3' }" indicators
+    <UCarousel ref="carouselRef" v-if="modelList" v-slot="{ item }" :items="modelList" :ui="{ item: 'basis-full md:basis-1/2 lg:basis-1/3' }" indicators
         class="banner-clip-path rounded-lg overflow-hidden bg-white min-h-[300px]" arrows>
         <img :src="getModelImagePublicUrl(item.main_img)" class="w-full" draggable="false" @click="handleClick(item.id)">
     </UCarousel>
@@ -10,9 +10,27 @@ import { type Model } from "~/types/model"
 defineProps<{
     modelList:Model[],
 }>()
+
+onMounted(()=>{
+    autoPlay()
+})
+
 const { getModelImagePublicUrl } = useSupabase()
+const carouselRef = ref()
+
 function handleClick(id:string){
     navigateTo(`/MyModel/ModelDetail-${id}`)
+}
+function autoPlay(){
+    setInterval(() => {
+    if (!carouselRef.value) return
+
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0)
+    }
+
+    carouselRef.value.next()
+  }, 3000)
 }
 
 </script>
