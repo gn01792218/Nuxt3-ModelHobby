782500c722f58fa666ec40f11c18ec2723db9a8b
MSI\gn01792218 <gn01792218@hotmail.com>
2024-04-08 17:31:21 +0800

    [feature] add thumbs controll another swiper

--- components/Swiper/Thumbs.vue
+++ components/Swiper/Thumbs.vue
@@ -1,30 +1,60 @@
 <template>
-    <Swiper 
-    class="w-full max-w-[1280px] h-[200px] xs:h-[400px] sm:h-[500px]"
-    :modules="[SwiperFreeMode, SwiperThumbs, SwiperPagination]" :style="{
-        '--swiper-navigation-color': '#fff',
-        '--swiper-pagination-color': '#fff',
-    }"
-    :pagination="{
-        clickable:true
-    }" 
-    :loop="true" :spaceBetween="10" :thumbs="{ swiper: thumbsSwiper }">
-        <SwiperSlide class="relative" v-for="slide in sliderItems" :key="slide" @click="processOpenImgPanel(getFinishImagePublicUrl(slide))">
-            <img class="w-full h-full object-contain" :src="getFinishImagePublicUrl(slide)"
-                :alt="slide">
+    <Swiper class="w-full max-w-[1280px] h-[200px] xs:h-[400px] sm:h-[500px]"
+        :modules="[SwiperFreeMode, SwiperThumbs, SwiperPagination, SwiperController]" 
+        @swiper="setMainSwiper"
+        @activeIndexChange="setCurrentIndex"
+        :thumbs="{ swiper: thumbsSwiper }"
+        :style="{
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+        }" 
+        :pagination="{
+            clickable: true
+        }" 
+        :spaceBetween="10" 
+        >
+        <SwiperSlide class="relative" v-for="slide in sliderItems" :key="slide"
+            @click="processOpenImgPanel()">
+            <img class="w-full h-full object-contain" :src="getFinishImagePublicUrl(slide)" :alt="slide">
         </SwiperSlide>
     </Swiper>
-    <Swiper
-    class="border-2 w-full h-[100px] max-w-[1280px] sm:h-[150px] md:h-[200px] swiper-thumbs "
-     :modules="[SwiperFreeMode, SwiperThumbs, SwiperPagination, SwiperNavigation]"
-        @swiper="setThumbsSwiper" :loop="true" :navigation="true" :spaceBetween="10" :slidesPerView="4" :freeMode="true"
-        :watchSlidesProgress="true">
+    <Swiper class="border-2 w-full h-[100px] max-w-[1280px] sm:h-[150px] md:h-[200px] swiper-thumbs "
+        :modules="[SwiperFreeMode, SwiperThumbs, SwiperPagination, SwiperNavigation]" 
+        @swiper="setThumbsSwiper"
+        :navigation="true" 
+        :spaceBetween="10" 
+        :slidesPerView="4" 
+        :freeMode="true"
+        :watchSlidesProgress="true"
+        >
         <SwiperSlide class="relative cursor-pointer" v-for="slide in sliderItems" :key="slide">
-            <img class="w-full h-full opacity-50 hover:opacity-100" :src="getFinishImagePublicUrl(slide)"
-                :alt="slide">
+            <img class="w-full h-full opacity-50 hover:opacity-100" :src="getFinishImagePublicUrl(slide)" :alt="slide">
         </SwiperSlide>
     </Swiper>
-    <ImgModal :img="viewImg"/>
+    <UModal class="w-full" v-model="openImgPanel" 
+        :ui="{
+            container: 'items-center'
+        }"
+    >
+        <Swiper class="w-full max-w-[1280px] h-[200px] xs:h-[400px] sm:h-[500px]"
+        :modules="[SwiperFreeMode, SwiperPagination, SwiperController]"
+        :initialSlide="currentIndex" 
+        :controller="{ control: mainSwiper }"
+        :style="{
+            '--swiper-navigation-color': '#fff',
+            '--swiper-pagination-color': '#fff',
+        }" 
+        :pagination="{
+            clickable: true
+        }" 
+        :spaceBetween="10" 
+        :zoom="true"
+        >
+        <SwiperSlide class="relative" v-for="slide in sliderItems" :key="slide">
+            <img class="w-full h-full object-contain" :src="getFinishImagePublicUrl(slide)" :alt="slide">
+        </SwiperSlide>
+    </Swiper>
+    </UModal>
 </template>
 
 <script setup lang="ts">
@@ -37,16 +67,27 @@ defineProps<{
 
 const { getFinishImagePublicUrl } = useSupabase()
 const { setOpenImgPanel } = useMyModelStore()
+const { openImgPanel } = storeToRefs(useMyModelStore())
 
-const thumbsSwiper = ref(null);
-const viewImg = ref('')
+const thumbsSwiper = ref<Swiper | null>(null);
+const mainSwiper = ref<Swiper | null>(null);
+const currentIndex = ref<number>(0);
 
-const setThumbsSwiper = (swiper:any) => {
+const setMainSwiper = (swiper:any) =>{
+    mainSwiper.value = swiper
+}
+const setThumbsSwiper = (swiper: any) => {
     thumbsSwiper.value = swiper;
 };
-function processOpenImgPanel(img:string){
-    setOpenImgPanel(true)
-    viewImg.value = img
-}
 
+// 由於Modal中的Swiper會每次都重生( NuxtUI的modal機制 )
+// 所以要控制modal中的swiper index就得透過 initialSlide 參數來設置
+// 這裡當主要的swiper index有變動時，就會更改currentIndex
+
+const onMainSwiperActiveIndexChange = (swiper:Swiper)=>{
+    currentIndex.value = swiper.activeIndex
+}
+function processOpenImgPanel() {
+    setOpenImgPanel(true)
+}
 </script>
