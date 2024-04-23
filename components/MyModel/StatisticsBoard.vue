781d1cf4328eb124e82e79d742f70088d2d07400
MSI\gn01792218 <gn01792218@hotmail.com>
2024-04-23 23:55:29 +0800

    [log] add log2

--- components/MyModel/StatisticsBoard.vue
+++ components/MyModel/StatisticsBoard.vue
@@ -11,9 +11,9 @@
         <USelect class="cursor-pointer" v-model="purchaseDate" :options="purchaseDateSelects" option-attribute="name" variant="none" @change="onPurchaseDateChange"/>
     </UDivider>
     <div>
-        <!-- <p>本月花費 <span class="text-acent-500">{{ thisMonthPurchaseCoast }}</span>元</p>
+        <p>本月花費 <span class="text-acent-500">{{ thisMonthPurchaseCoast }}</span>元</p>
         <p class="cursor-pointer" @click="openModelsDetailModal(thisMonthFinishedModels)">已完成<span
-                class="text-green-500">{{ thisMonthFinishedCount }}</span>個模型</p> -->
+                class="text-green-500">{{ thisMonthFinishedCount }}</span>個模型</p>
         <!-- <p class="cursor-pointer" @click="openModelsDetailModal(thisMonthPurchaseModels)">本月購入了<span
                 class="text-green-800">{{ thisMonthPurchaseModelsCount }}個模型</span></p> -->
         <!-- <p>模型完成數量順逆差 :
