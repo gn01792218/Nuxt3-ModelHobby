b6ced2905bd50c3048a78511d4d6b3849b5c5382
MSI\gn01792218 <gn01792218@hotmail.com>
2024-05-06 12:05:16 +0800

    [refactor] refactor Gallery Thumbs image performence

--- components/Swiper/Thumbs.vue
+++ components/Swiper/Thumbs.vue
@@ -8,14 +8,14 @@
             clickable: true
         }" :spaceBetween="10">
         <SwiperSlide class="relative" v-for="slide in sliderItems" :key="slide" @click="processOpenImgPanel()">
-            <NuxtImg class="w-full h-full object-contain" format="webp" width="600" :src="getFinishImagePublicUrl(slide)" :alt="slide"/>
+            <NuxtImg preload class="w-full h-full object-contain" format="webp" sizes="380px md:500px" :src="getFinishImagePublicUrl(slide)" :alt="slide"/>
         </SwiperSlide>
     </Swiper>
     <Swiper class="border-2 w-full h-[100px] max-w-[1280px] sm:h-[150px] md:h-[200px] swiper-thumbs "
         :modules="[SwiperFreeMode, SwiperThumbs, SwiperPagination, SwiperNavigation]" @swiper="setThumbsSwiper"
         :navigation="true" :spaceBetween="10" :slidesPerView="4" :freeMode="true" :watchSlidesProgress="true">
         <SwiperSlide class="relative cursor-pointer" v-for="slide in sliderItems" :key="slide">
-            <NuxtImg class="w-full h-full opacity-50 hover:opacity-100" format="webp" width="300" :src="getFinishImagePublicUrl(slide)" :alt="slide"/>
+            <NuxtImg class="w-full h-full opacity-50 hover:opacity-100" format="webp" sizes="87px md:300px" :src="getFinishImagePublicUrl(slide)" :alt="slide"/>
         </SwiperSlide>
     </Swiper>
     <UModal class="w-full flex items-center" v-model="openImgPanel" :ui="{
@@ -42,7 +42,7 @@
             :autoHeight="true"
             >
             <SwiperSlide class="w-full" v-for="slide in sliderItems" :key="slide">
-                <NuxtImg class="w-full h-full object-contain" format="webp" width="600" :quality="100" :src="getFinishImagePublicUrl(slide)" :alt="slide"/>
+                <NuxtImg loading="lazy" class="w-full h-full object-contain" format="webp" sizes="400px md:600px" :quality="100" :src="getFinishImagePublicUrl(slide)" :alt="slide"/>
             </SwiperSlide>
         </Swiper>
     </UModal>
@@ -74,7 +74,6 @@ const setThumbsSwiper = (swiper: any) => {
 // 由於Modal中的Swiper會每次都重生( NuxtUI的modal機制 )
 // 所以要控制modal中的swiper index就得透過 initialSlide 參數來設置
 // 這裡當主要的swiper index有變動時，就會更改currentIndex
-
 const onMainSwiperActiveIndexChange = (swiper: Swiper) => {
     currentIndex.value = swiper.activeIndex
 }
