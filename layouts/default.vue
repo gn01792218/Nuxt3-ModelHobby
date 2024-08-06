e35474e7adcb3dda637ccd27ce7d06ac6b225448
MSI\gn01792218 <gn01792218@hotmail.com>
2024-08-06 17:13:21 +0800

    [feature] add meta des to imporve SEO

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,9 @@
 
 <script lang="ts" setup>
 useHead({
-    title: 'Hobby 模時間 Time'
+    title: 'Hobby 模時間 Time',
+    meta: [
+    { name: 'description', content: '模型山積越來越多?!同樣的模型不小心買了幾百個?!作品沒有展示的空間?!喜愛動手的膠佬們有福啦!這個網站可以幫助你控管你的模型庫存、購買資訊等等!還在等什麼，快點加入或看看其他作者的作品吧!' }
+  ],
 })
 </script>
--- layouts/login.vue
+++ layouts/login.vue
@@ -4,6 +4,9 @@
 
 <script lang="ts" setup>
 useHead({
-    title: 'HobbyTime 使用者登入'
+    title: 'HobbyTime 使用者登入',
+    meta: [
+    { name: 'description', content: '模型山積越來越多?!同樣的模型不小心買了幾百個?!作品沒有展示的空間?!喜愛動手的膠佬們有福啦!這個網站可以幫助你控管你的模型庫存、購買資訊等等!還在等什麼，快註冊啊啊啊啊!!!' }
+  ],
 })
 </script>
