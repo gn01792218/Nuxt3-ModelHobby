18b8c7846f6f30d5bff80a8e70c95fc46cf8cfd8
MSI\gn01792218 <gn01792218@hotmail.com>
2024-09-05 16:38:42 +0800

    [fix] fix scroll to top style bug

--- components/ScrollToTop.vue
+++ components/ScrollToTop.vue
@@ -1,6 +1,6 @@
 <template>  
   <div class="fixed bottom-4 right-4">  
-    <button @click="scrollToTop" :class="[isVisible? 'translate-x-[0px]':'']" class="w-[100px] p-2 transition-all duration-1000 ease-in-out translate-x-full bg-blue-600 text-white rounded-full shadow-lg">  
+    <button @click="scrollToTop" :class="[isVisible? 'translate-x-[35px]':'']" class="w-[100px] p-2 transition-all duration-1000 ease-in-out translate-x-full bg-main-600 text-white rounded-full shadow-lg">  
         <UIcon name="i-heroicons-arrow-up-circle-16-solid"/>
     </button>  
   </div>  
@@ -13,6 +13,7 @@ const isVisible = ref(false);
 
 const handleScroll = () => {  
   isVisible.value = window.scrollY > 250; // 阈值，可以根据需要调整  
+  console.log(isVisible.value)
 };  
 
 const scrollToTop = () => {  
